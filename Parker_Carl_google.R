ef0ec14a1b0451dac69df20b7c018b562b6108b7
Carl Parker <carl.parker@hbo.com>
2017-05-08 18:17:47 -0700

    shorten the var name for readability

--- Parker_Carl_google.R
+++ Parker_Carl_google.R
@@ -5,13 +5,8 @@
 library( magrittr )
 library( googlesheets )
 
-
-#
-# Update the following line with the name of the CSV file to read.
-#
-electoral.votes.file.ch <- "data/electoral-college.csv"
 electoral.votes.df <- read.csv( 
-                         electoral.votes.file.ch, 
+                        "data/electoral-college.csv",
                          header = FALSE, 
                          quote="\"",
                          sep = ",", 
@@ -23,10 +18,8 @@ electoral.votes.df <- read.csv(
                        )
 
 names( electoral.votes.df ) <- c( "Electoral.Votes", "State" )
-electoral.votes.df$State <- gsub( "\\*", "", electoral.votes.df$State )
-head( electoral.votes.df )
+electoral.votes.df$State    <- gsub( "\\*", "", electoral.votes.df$State )
 
-sum( electoral.votes.df$Electoral.Votes )
 
 official.gs <- gs_url( c( "https://docs.google.com/spreadsheets/d/133Eb4qQmOxNvtesw2hdVns073R68EZx4SfCnP4IGQf8/" ) ) 
 
@@ -35,25 +28,25 @@ official.gs %>% gs_read( range = "A12:D24" )
 swing.df   <- official.gs %>% gs_read( range = "A12:D24", col_names = FALSE ) %>% data.frame()
 noswing.df <- official.gs %>% gs_read( range = "A27:D64", col_names = FALSE ) %>% data.frame()
 
-votes.all.states.df <- rbind( swing.df, noswing.df )
-names( votes.all.states.df  )<- c( "State", "Clinton", "Trump", "Other" )
-head( votes.all.states.df )
-votes.all.states.df$State <- gsub( "\\*", "", votes.all.states.df$State )
+votes.df <- rbind( swing.df, noswing.df )
+names( votes.df  )<- c( "State", "Clinton", "Trump", "Other" )
+head( votes.df )
+votes.df$State <- gsub( "\\*", "", votes.df$State )
 
-pop.elect.votes.all.states.df <- merge( votes.all.states.df, electoral.votes.df )
-head( pop.elect.votes.all.states.df )
+pop.elect.votes.df <- merge( votes.df, electoral.votes.df )
+head( pop.elect.votes.df )
 
-sum( pop.elect.votes.all.states.df[ votes.all.states.df$Trump > votes.all.states.df$Clinton, ]$Electoral.Votes )
-sum( pop.elect.votes.all.states.df[ votes.all.states.df$Trump < votes.all.states.df$Clinton, ]$Electoral.Votes )
+sum( pop.elect.votes.df[ votes.df$Trump > votes.df$Clinton, ]$Electoral.Votes )
+sum( pop.elect.votes.df[ votes.df$Trump < votes.df$Clinton, ]$Electoral.Votes )
 
-votes.all.states.df$trump.one.pct <- floor( votes.all.states.df$Trump * 0.1 )
-votes.all.states.df$Clinton.retally <- votes.all.states.df$Clinton + votes.all.states.df$trump.one.pct
-votes.all.states.df$Trump.retally   <- votes.all.states.df$Trump   - votes.all.states.df$trump.one.pct
+votes.df$trump.one.pct <- floor( votes.df$Trump * 0.1 )
+votes.df$Clinton.retally <- votes.df$Clinton + votes.df$trump.one.pct
+votes.df$Trump.retally   <- votes.df$Trump   - votes.df$trump.one.pct
 
-votes.all.states.df$winner.retally  <- ifelse( votes.all.states.df$Trump.retally > votes.all.states.df$Clinton.retally, "Trump", "Clinton" )
+votes.df$winner.retally  <- ifelse( votes.df$Trump.retally > votes.df$Clinton.retally, "Trump", "Clinton" )
 
-sum( pop.elect.votes.all.states.df[ votes.all.states.df$Trump.retally > votes.all.states.df$Clinton.retally, ]$Electoral.Votes )
-sum( pop.elect.votes.all.states.df[ votes.all.states.df$Trump.retally < votes.all.states.df$Clinton.retally, ]$Electoral.Votes )
+sum( pop.elect.votes.df[ votes.df$Trump.retally > votes.df$Clinton.retally, ]$Electoral.Votes )
+sum( pop.elect.votes.df[ votes.df$Trump.retally < votes.df$Clinton.retally, ]$Electoral.Votes )
 
 #
 # NYT Election Results
