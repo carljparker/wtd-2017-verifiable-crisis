f82942b08ec1c546d51d2422ac21dbe8b7c0f948
Carl Parker <carl.parker@hbo.com>
2017-05-09 18:31:42 -0700

    using lapply and getting there...

--- get-electoral-votes.R
+++ get-electoral-votes.R
@@ -9,8 +9,15 @@ wiki.elect.votes <-  paste(
                           )
 
 evotes.by.state.li <- read_html( wiki.elect.votes ) %>%
-  html_nodes( "table.wikitable:nth-child(103)" ) %>% 
+  html_nodes( "table.wikitable:nth-child(103)" )    %>% 
   html_table()
 
 evotes.by.state.df <- evotes.by.state.li[[1]]
+names( evotes.by.state.df ) <- c( "EV", "State" )
+
+evotes.by.state.df <- evotes.by.state.df[ -( 20 ), ] 
+evotes.by.state.df$EV <- strsplit( evotes.by.state.df$EV, " " )
+
+lapply( evotes.by.state.df$EV, function( li ) as.numeric( unlist( li )[ 1 ] ) )
+evotes.by.state.df$EV <- as.numeric( unlist( evotes.by.state.df$EV )[ 1 ] )
 
