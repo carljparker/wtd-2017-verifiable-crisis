ca3b1853f0ddcd2da0817a1a0913d57c51dd8c01
Carl Parker <carl.parker@hbo.com>
2017-05-10 21:58:31 -0700

    numerous changes...among them, add a css

--- get-electoral-votes.R
+++ get-electoral-votes.R
@@ -1,5 +1,6 @@
 
 library( rvest )
+library( knitr )
 
 wiki.elect.votes <-  paste( 
                             "https://en.wikipedia.org/wiki/",
@@ -13,25 +14,25 @@ evotes.wiki.li <- read_html( wiki.elect.votes ) %>%
   html_table()
 
 evotes.wiki.df <- evotes.wiki.li[[1]]
-names( evotes.wiki.df ) <- c( "EV", "State" )
-
 evotes.wiki.df <- evotes.wiki.df[ -( 20 ), ] 
+
+names( evotes.wiki.df ) <- c( "EV", "States" )
+
 evotes.wiki.df$EV <- strsplit( evotes.wiki.df$EV, " " )
 
 evotes.wiki.df$EV <- unlist( lapply( evotes.wiki.df$EV, function( li ) as.numeric( unlist( li )[ 1 ] ) ) )
 
 evotes.df <- data.frame()
-row <- 1
-
-states.vec <- trimws( unlist( strsplit( evotes.wiki.df[ row, 2 ], "," ) ) )
-rbind( evotes.df, cbind( states.vec, evotes.wiki.df[ row, 1] ) )
-evotes.df
 
 for( row.idx in 1:nrow( evotes.wiki.df ) ) {
   states.vec <- trimws( unlist( strsplit( evotes.wiki.df[ row.idx, 2 ], "," ) ) )
   evotes.df <- rbind( evotes.df, cbind( states.vec, evotes.wiki.df[ row.idx, 1] ) )
 }
 
-evotes.df
+names( evotes.df ) <- c( "State", "Electoral.Votes" )
+
+knitr::kable( head( evotes.df ) )
+
+
 
 
