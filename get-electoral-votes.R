c5bb142d618aa2f62ec4750de1b7ea243fab39f7
Carl Parker <carl.parker@hbo.com>
2017-05-10 17:59:23 -0700

    just about got it

--- get-electoral-votes.R
+++ get-electoral-votes.R
@@ -8,15 +8,30 @@ wiki.elect.votes <-  paste(
                             sep = ""
                           )
 
-evotes.by.state.li <- read_html( wiki.elect.votes ) %>%
+evotes.wiki.li <- read_html( wiki.elect.votes ) %>%
   html_nodes( "table.wikitable:nth-child(103)" )    %>% 
   html_table()
 
-evotes.by.state.df <- evotes.by.state.li[[1]]
-names( evotes.by.state.df ) <- c( "EV", "State" )
+evotes.wiki.df <- evotes.wiki.li[[1]]
+names( evotes.wiki.df ) <- c( "EV", "State" )
 
-evotes.by.state.df <- evotes.by.state.df[ -( 20 ), ] 
-evotes.by.state.df$EV <- strsplit( evotes.by.state.df$EV, " " )
+evotes.wiki.df <- evotes.wiki.df[ -( 20 ), ] 
+evotes.wiki.df$EV <- strsplit( evotes.wiki.df$EV, " " )
 
-evotes.by.state.df$EV <- unlist( lapply( evotes.by.state.df$EV, function( li ) as.numeric( unlist( li )[ 1 ] ) ) )
+evotes.wiki.df$EV <- unlist( lapply( evotes.wiki.df$EV, function( li ) as.numeric( unlist( li )[ 1 ] ) ) )
+
+evotes.df <- data.frame()
+row <- 1
+
+states.vec <- trimws( unlist( strsplit( evotes.wiki.df[ row, 2 ], "," ) ) )
+rbind( evotes.df, cbind( states.vec, evotes.wiki.df[ row, 1] ) )
+evotes.df
+
+for( row.idx in 1:nrow( evotes.wiki.df ) ) {
+  states.vec <- trimws( unlist( strsplit( evotes.wiki.df[ row.idx, 2 ], "," ) ) )
+  evotes.df <- rbind( evotes.df, cbind( states.vec, evotes.wiki.df[ row.idx, 1] ) )
+}
+
+evotes.df
+
 
